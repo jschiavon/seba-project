560889d54c62c85f6fc379cef9bb9e2b84a39f29
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2022-02-06 10:35:21 +0100

    improved windows usability of the script

--- generate_images.py
+++ generate_images.py
@@ -3,12 +3,10 @@ import argparse
 
 from libimgs import *
 
-
 parser = argparse.ArgumentParser(description="Generate a grid of randomly rotated images.",
                                  epilog="""The images in the image folder should be named 'X.jpg' with X a 2-digits
                                         padded number starting from '01.jpg'. Dealing with formats different from 
                                         jpg is still not implemented.""")
-
 parser.add_argument('images', type=int, nargs='+',
                     help='the numbers of the selected images')
 parser.add_argument('--folder', type=str, default='img',
@@ -24,17 +22,18 @@ parser.add_argument('-v', '--verbose', action='store_true',
 parser.add_argument('-r', '--repeat', type=int, default=1, dest='rep',
                     help='how many repeating of the generation (default 1)')
 
-args = parser.parse_args()
+if __name__ == '__main__':
+    args = parser.parse_args()
 
-images = [join(args.folder, f"{i:02}.jpg") for i in args.images]  # Selected images
+    images = [join(args.folder, f"{i:02}.jpg") for i in args.images]  # Selected images
 
-M = args.rows  # Number of rows
-N = args.cols  # Number of columns
+    M = args.rows  # Number of rows
+    N = args.cols  # Number of columns
 
-if args.rep > 1:
-    for i in range(args.repeat):
+    if args.rep > 1:
+        for i in range(args.repeat):
+            grid = create_grid(images, M, N, verbose=args.verbose)
+            plot_grid(grid, out=join(args.folder, f"{args.output}_{i}.jpg"))
+    else:
         grid = create_grid(images, M, N, verbose=args.verbose)
-        plot_grid(grid, out=join(args.folder, f"{args.output}_{i}.jpg"))
-else:
-    grid = create_grid(images, M, N, verbose=args.verbose)
-    plot_grid(grid, out=join(args.folder, f"{args.output}.jpg"))
+        plot_grid(grid, out=join(args.folder, f"{args.output}.jpg"))
